2965c7e2f06e4dd5a5d351a46ad64fb208eed494
zsedrick <zsedrickjavier03@gmail.com>
2025-04-24 22:11:33 +0800

    "Adding person object for practice"

---
+++ script.js
@@ -0,0 +1,35 @@
+//#region Creating a object
+// let person = {
+//   name: null,
+//   age: null,
+//   location: null,
+//   id: [
+//   {
+//     driverLicense: null, nationalId: null
+//   },
+//   {
+//     driverLicense: null, nationalId: null
+//   }
+//   ],
+
+//   printInfo(){
+//     console.log(`Name:${this.name} Age:${this.age} Location:${this.location} `);
+//     this.id.forEach(ids => {
+//       console.log(`Drivers License: ${ids.driverLicense},National Id: ${ids.nationalId}`);
+//     }
+//     )
+//   }
+// }
+
+
+// person.name = 'zsed';
+// person.age = 21;
+// person.location = 'kaybagal';
+// person.id.forEach(ids => {
+//   ids.driverLicense = true;
+//   ids.nationalId = false;
+// })
+
+// person.printInfo();
+//#endregion
+
